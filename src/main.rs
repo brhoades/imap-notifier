ff264d8caaa75679eb6b9e90c743e316476ff8e4
Billy Rhoades <me@brod.es>
2020-01-28 18:48:53 -0800

    Using tokio streams, threads now work!

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,22 @@ use std::io::BufReader;
 use std::net::ToSocketAddrs;
 use std::path::PathBuf;
 use std::sync::Arc;
-use std::task::Poll::{Pending, Ready};
 use structopt::StructOpt;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
 use tokio_rustls::{rustls::ClientConfig, webpki::DNSNameRef, TlsConnector};
 
-use futures::future::FutureExt;
 use serde::Deserialize;
 
-use slog::{debug, error, info, o, trace, Logger};
+use slog::{debug, error, info, o, Logger};
 
 use failure::{format_err, Error};
 
-enum State<'a, 'b, 'c> {
+enum State<'a, 'b> {
     Login(&'a String, &'b String),
     WaitAuthenticated,
-    SelectFolder(&'c String),
     WaitSelected,
-    RequestIdle,
     WaitIdle,
     Idle,
 }
@@ -105,7 +101,7 @@ fn main() -> Result<(), Error> {
 
     // let mut tasks: Vec<_> = vec![];
     let script = opt.script.clone();
-    let futs = cfg
+    let tasks = cfg
         .accounts
         .iter()
         .map(|account| {
@@ -115,8 +111,8 @@ fn main() -> Result<(), Error> {
             account.folders.iter().zip(accounts).map(|(folder, acct)| {
                 rt.spawn(watch(
                     // root.new(o!("name" => format!("{} on {}", folder.name, &folder_acct.host))),
-                    root.new(o!("name" => format!("{} on {}", folder.name, acct.host))),
-                    acct,
+                    root.new(o!("folder" => folder.clone().name, "host" => acct.clone().host)),
+                    acct.clone(),
                     folder.name.clone(),
                     script.clone(),
                     config.clone(),
@@ -126,26 +122,19 @@ fn main() -> Result<(), Error> {
         .flatten()
         .collect::<Vec<_>>(); // drain to evaluate lazy iter
 
-    for f in futs {
-        println!("RUNNING FUTURE");
-        rt.block_on(f)?.unwrap();
-    }
+    let mut size = tasks.len();
 
-    // let mut size = tasks.len();
-
-    /*
     let mut handle = futures::future::select_all(tasks);
     // https://docs.rs/futures-preview/0.3.0-alpha.18/futures/future/fn.select_all.html
     // 0 is the future that finished, futs is the reaiming ones.
     while size > 0 {
         debug!(root, "{} tasks remaining", size);
-        let (fut, new_size, futs) = handle.await;
+        let (fut, new_size, futs) = rt.block_on(handle);
         size = new_size;
         fut?.unwrap();
 
         handle = futures::future::select_all(futs);
     }
-     */
 
     // Wait until the runtime becomes idle and shut it down.
     Ok(())
@@ -161,10 +150,10 @@ async fn watch<'a>(
 ) -> Result<(), Error> {
     info!(logger, "starting watcher: {} on {}", folder, account.host);
 
-    let (host, port, fullhost): (&str, &str, &str) = if !account.host.contains(":") {
+    let (host, _port, fullhost): (&str, &str, &str) = if !account.host.contains(":") {
         (account.host.as_str(), "993", &account.host)
     } else {
-        let mut parts = account.host.split(':').collect::<Vec<_>>();
+        let parts = account.host.split(':').collect::<Vec<_>>();
         if parts.len() != 2 {
             info!(logger, "expected exactly two host parts: {:?}", parts);
             return Err(format_err!(
@@ -172,7 +161,6 @@ async fn watch<'a>(
                 parts.len(),
                 account.host
             ));
-            debug!(logger, "parts: {:?}", parts);
         }
 
         let (host, port) = (parts[0], parts[1]);
@@ -191,157 +179,103 @@ async fn watch<'a>(
     let stream = TlsConnector::from(config)
         .connect(domain, TcpStream::connect(host).await?)
         .await?;
-    let (mut read, mut write) = tokio::io::split(stream);
+    let (read, mut write) = tokio::io::split(stream);
     debug!(logger, "connected!");
-    let mut buff: Vec<u8> = vec![];
-    let mut first_time = true;
 
-    // Stream of Item = String separated by newlines.
-    let read_stream = futures::stream::poll_fn(move |ctx| {
-        use std::future::Future;
+    use tokio::stream::StreamExt;
 
-        println!("start inner: {}", String::from_utf8(buff.clone()).unwrap());
+    let mut state = State::Login(&account.user, &account.pass);
 
-        match Future::poll(std::pin::Pin::new(&mut read.read_u8()), ctx) {
-            Pending => {
-                // need to ensure input for the first state transition.
-                println!("pending");
-                if first_time {
-                    first_time = false;
-                    Ready(Some("".to_owned()))
-                } else {
-                    Pending
-                }
+    let mut stream = tokio::io::BufReader::new(read).lines().map(|line| {
+        if line.is_err() {
+            error!(logger, "-> ERR: {:?}", line.err());
+            return None;
+        }
+        let line = line.unwrap();
+
+        info!(logger, "-> {}", line);
+
+        if line.starts_with(". NO [AUTHENTICATIONFAILED]") {
+            error!(logger, "Authentication failed, quitting.");
+            return Some(". CLOSE".to_owned());
+        }
+
+        if line.starts_with(". BAD") {
+            error!(logger, "Sent an invalid command.");
+            return Some(". CLOSE".to_owned());
+        }
+
+        if line.starts_with("* BYE") {
+            info!(logger, "Remote said goodbye.");
+            panic!("byte");
+        }
+
+        use State::*;
+        match state {
+            Login(user, pass) => {
+                state = WaitAuthenticated;
+                return Some(format!(". LOGIN \"{}\" \"{}\"", user, pass));
             }
-            Ready(Err(e)) => {
-                println!("ERROR: {}", e);
-                Pending
-            }
-            Ready(Ok(c)) => {
-                println!("IN: {}", String::from_utf8(vec![c]).unwrap());
-
-                if c == '\r' as u8 || c == '\n' as u8 {
-                    if buff.len() > 0 {
-                        let output = String::from_utf8(buff.clone()).ok().unwrap();
-                        buff.clear();
-                        println!("ready: {}", output);
-                        Ready(Some(output))
-                    } else {
-                        Pending
-                    }
-                } else {
-                    buff.push(c);
-                    println!(
-                        "pushed {}, now: {}",
-                        c,
-                        String::from_utf8(buff.clone()).unwrap()
-                    );
-                    Pending
+            WaitAuthenticated => {
+                if line.contains("Logged in") {
+                    state = WaitSelected;
+                    return Some(". select ".to_owned() + &folder);
                 }
+
+                return None;
+            }
+            WaitSelected => {
+                if line.contains("Select completed") {
+                    debug!(logger, "waiting for idling confirmation");
+                    state = WaitIdle;
+                    return Some(". idle".to_owned());
+                }
+
+                return None;
+            }
+            WaitIdle => {
+                if line.contains("+ idling") {
+                    debug!(logger, "idle confirmed");
+                    state = Idle;
+                }
+
+                return None;
+            }
+            Idle => {
+                if line.contains("FETCH (FLAGS") {
+                    debug!(logger, "Calling script");
+                    let output = call_command(
+                        &script,
+                        vec![
+                            "FLAGS",
+                            &account.user,
+                            &folder,
+                            &get_flags(&line).unwrap().unwrap(),
+                        ],
+                    )
+                    .unwrap();
+
+                    debug!(logger, "STDOUT: {}", output.0);
+                    debug!(logger, "STDERR: {}", output.1);
+                }
+
+                if line.contains(" EXISTS") {
+                    debug!(logger, "Calling script");
+                    let output =
+                        call_command(&script, vec!["EXISTS", &account.user, &folder]).unwrap();
+
+                    debug!(logger, "STDOUT: {}", output.0);
+                    debug!(logger, "STDERR: {}", output.1);
+                }
+
+                return None;
             }
         }
     });
 
-    use futures_util::stream::StreamExt;
-
-    let mut state = State::Login(&account.user, &account.pass);
-    let mut stream = read_stream
-        .then(|line| {
-            info!(logger, "-> {}", line);
-
-            if line.starts_with(". NO [AUTHENTICATIONFAILED]") {
-                error!(logger, "Authentication failed, quitting.");
-                return futures::future::ok(Some(". CLOSE".to_owned()));
-            }
-
-            if line.starts_with(". BAD") {
-                error!(logger, "Sent an invalid command.");
-                return futures::future::ok(Some(". CLOSE".to_owned()));
-            }
-
-            if line.starts_with("* BYE") {
-                info!(logger, "Remote said goodbye.");
-                return futures::future::err(format_err!("remote hung up"));
-            }
-
-            use State::*;
-            match state {
-                Login(user, pass) => {
-                    state = WaitAuthenticated;
-                    return futures::future::ok(Some(format!(". LOGIN \"{}\" \"{}\"", user, pass)));
-                }
-                WaitAuthenticated => {
-                    state = if line.contains("Logged in") {
-                        SelectFolder(&folder)
-                    } else {
-                        WaitAuthenticated
-                    };
-                    return futures::future::ok(None);
-                }
-                SelectFolder(folder) => {
-                    state = WaitSelected;
-                    return futures::future::ok(Some(". select ".to_owned() + folder));
-                }
-                WaitSelected => {
-                    state = if line.contains("Select completed") {
-                        RequestIdle
-                    } else {
-                        WaitSelected
-                    };
-                    return futures::future::ok(None);
-                }
-                RequestIdle => {
-                    state = WaitIdle;
-                    info!(logger, "waiting for idling confirmation");
-
-                    return futures::future::ok(Some(". idle".to_owned()));
-                }
-                WaitIdle => {
-                    state = if line.contains("+ idling") {
-                        info!(logger, "idle confirmed");
-                        Idle
-                    } else {
-                        WaitIdle
-                    };
-
-                    return futures::future::ok(None);
-                }
-                Idle => {
-                    if line.contains("FETCH (FLAGS") {
-                        info!(logger, "Calling script");
-                        let output = call_command(
-                            &script,
-                            vec![
-                                "FLAGS",
-                                &account.user,
-                                &folder,
-                                &get_flags(&line).unwrap().unwrap(),
-                            ],
-                        )
-                        .unwrap();
-
-                        debug!(logger, "STDOUT: {}", output.0);
-                        debug!(logger, "STDERR: {}", output.1);
-                    }
-
-                    if line.contains(" EXISTS") {
-                        info!(logger, "Calling script");
-                        let output =
-                            call_command(&script, vec!["EXISTS", &account.user, &folder]).unwrap();
-
-                        debug!(logger, "STDOUT: {}", output.0);
-                        debug!(logger, "STDERR: {}", output.1);
-                    }
-
-                    return futures::future::ok(None);
-                }
-            }
-        })
-        .filter(|v| futures::future::ready(!(v.is_ok() && v.as_ref().unwrap().is_none())));
-
     while let Some(res) = stream.next().await {
         match res {
-            Ok(Some(out)) => {
+            Some(out) => {
                 let len = out.len();
                 info!(logger, "<- {}", out.clone());
                 let size = write.write((out + "\r\n").as_bytes()).await? - 2;
@@ -353,10 +287,11 @@ async fn watch<'a>(
                     ));
                 }
             }
-            Ok(None) => (),
+            None => (),
+            /*
             Err(e) => {
                 return Err(format_err!("err: {}", e));
-            }
+            }*/
         }
     }
 
